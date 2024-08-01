ee286edc3626390722b36e808292bf0b74778905
Jordan Sidney-Dunu <sidneydunuu@student.douglascollege.ca>
2024-07-31 21:03:49 -0700

    feat: added dist folder.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist' 
+    outDir: 'dist', // Specify the output directory
   },
-  server: {
-    proxy: {
-      '/api': 'https://recipes-app-9d6n-i95ckmxtk-jordiloos-projects.vercel.app/'
-    }
-  }
-})
+});
