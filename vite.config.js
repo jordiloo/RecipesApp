6459a8397d6477bd8bfc1864b2f0b754c2cb417a
Jordan Sidney-Dunu <sidneydunuu@student.douglascollege.ca>
2024-07-31 19:57:43 -0700

    feat: modified package.json file.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist' 
+  },
   server: {
     proxy: {
       '/api': 'https://recipes-app-9d6n-i95ckmxtk-jordiloos-projects.vercel.app/'
